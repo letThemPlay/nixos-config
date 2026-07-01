df1adc7dc8c05f209a09bf8b0b2f3e4cb86542b5
kelvin <kelvin.d.morris@gmail.com>
2026-07-01 12:04:28 +0100

    moved stylix schema into its own file

--- modules/_schemas/hosts.nix
+++ modules/_schemas/hosts.nix
@@ -34,36 +34,4 @@
       }
     );
   };
-
-  options.ltp.stylixOverrides = lib.mkOption {
-    description = "Strict typing container box for host-dependent visual scales and metrics.";
-    default = { };
-    type = lib.types.submodule {
-      options = {
-        cursorSize = lib.mkOption {
-          type = lib.types.int;
-          default = 24;
-        };
-        fontSize = lib.mkOption {
-          default = { };
-          type = lib.types.submodule {
-            options = {
-              terminal = lib.mkOption {
-                type = lib.types.int;
-                default = 11;
-              };
-              applications = lib.mkOption {
-                type = lib.types.int;
-                default = 12;
-              };
-              desktop = lib.mkOption {
-                type = lib.types.int;
-                default = 10;
-              };
-            };
-          };
-        };
-      };
-    };
-  };
 }
