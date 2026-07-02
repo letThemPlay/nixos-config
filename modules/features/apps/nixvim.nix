c0196a5b213359f7a61e841af75153f61eaff40b
kelvin <kelvin.d.morris@gmail.com>
2026-07-02 15:57:26 +0100

    Working on new nixvim config

--- modules/features/apps/nixvim.nix
+++ modules/features/apps/nixvim.nix
@@ -3,36 +3,86 @@
     {
       config,
       lib,
-      pkgs,
       ...
     }:
     let
       cfg = config.features.nixvim;
     in
     {
-      options.features.nixvim = {
-        enable = lib.mkEnableOption "Nixvim modular configuration workspace" // {
-          default = true;
-        };
+      options.features.nixvim.enable = lib.mkEnableOption "NixVim declarative Neovim environment" // {
+        default = false;
       };
 
       config = lib.mkIf cfg.enable {
-        environment.systemPackages = [
-          pkgs.ripgrep
-          pkgs.lazygit
-        ];
-
         home-manager.sharedModules = [
-          inputs.nixvim.homeModules.nixvim
+          inputs.nixvim.homeManagerModules.nixvim
           (_: {
-            programs.nixvim.nixpkgs.source = inputs.nixpkgs;
-          })
+            programs.nixvim = {
+              enable = true;
+              defaultEditor = true;
 
-          ./_nixvim/default.nix
-          ./_nixvim/options.nix
-          ./_nixvim/keymaps.nix
-          ./_nixvim/_plugins/core.nix
-          ./_nixvim/_plugins/lsp.nix
+              opts = {
+                number = true;
+                relativenumber = true;
+                shiftwidth = 2;
+                tabstop = 2;
+                expandtab = true;
+                smartindent = true;
+                termguicolors = true;
+                wrap = false;
+              };
+
+              globals.mapleader = " ";
+
+              plugins = {
+                treesitter.enable = true;
+
+                telescope = {
+                  enable = true;
+                  keymaps = {
+                    "<leader>ff" = "find_files";
+                    "<leader>fg" = "live_grep";
+                    "<leader>fb" = "buffers";
+                  };
+                };
+
+                lualine = {
+                  enable = true;
+                  settings.options.theme = "auto";
+                };
+
+                neo-tree = {
+                  enable = true;
+                  closeIfLastWindow = true;
+                };
+
+                nvim-autopairs.enable = true;
+              };
+
+              keymaps = [
+                {
+                  mode = "n";
+                  key = "<leader>e";
+                  action = "<cmd>Neotree toggle<cr>";
+                  options.desc = "Toggle Neo-tree Panel";
+                }
+                {
+                  mode = "n";
+                  key = "<leader>v";
+                  action = "<cmd>vsplit<cr>";
+                  options.desc = "Vertical Window Split";
+                }
+                {
+                  mode = "n";
+                  key = "<leader>h";
+                  action = "<cmd>split<cr>";
+                  options.desc = "Horizontal Window Split";
+                }
+              ];
+            };
+
+            stylix.targets.nixvim.enable = true;
+          })
         ];
       };
     };
