e90709b9fd13bd30ae5191126eee4271cd40ad5a
kelvin <kelvin.d.morris@gmail.com>
2026-07-02 19:59:15 +0100

    Adding cmp autocompletion and lsp into nixvim

--- modules/features/apps/nixvim.nix
+++ modules/features/apps/nixvim.nix
@@ -43,6 +43,7 @@
 
               plugins = {
                 treesitter.enable = true;
+                nvim-autopairs.enable = true;
 
                 telescope = {
                   enable = true;
@@ -70,20 +71,59 @@
                   };
                 };
 
-                nvim-autopairs.enable = true;
-
                 toggleterm = {
                   enable = true;
                   settings = {
                     open_mapping = "[[<C-/>]]";
-
                     direction = "horizontal";
                     size = 15;
-
                     insert_mappings = true;
                     terminal_mappings = true;
-
                     start_in_insert = true;
+                  };
+                };
+
+                lsp = {
+                  enable = true;
+                  servers = {
+                    nixd.enable = true;
+
+                    # Optional: Add any future languages effortlessly by setting to true
+                    # bashls.enable = true;
+                    # gopls.enable = true;
+                  };
+
+                  keymaps.lspBuf = {
+                    "gd" = "definition";
+                    "gr" = "references";
+                    "K" = "hover";
+                    "<leader>rn" = "rename";
+                    "<leader>ca" = "code_action";
+                  };
+                };
+
+                luasnip.enable = true;
+
+                cmp = {
+                  enable = true;
+                  settings = {
+                    snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+
+                    mapping = {
+                      "<C-Space>" = "cmp.mapping.complete()";
+                      "<C-e>" = "cmp.mapping.abort()";
+                      "<CR>" = "cmp.mapping.confirm({ select = true })";
+
+                      "<C-n>" = "cmp.mapping.select_next_item()";
+                      "<C-p>" = "cmp.mapping.select_prev_item()";
+                    };
+
+                    sources = [
+                      { name = "nvim_lsp"; }
+                      { name = "luasnip"; }
+                      { name = "path"; }
+                      { name = "buffer"; }
+                    ];
                   };
                 };
               };
@@ -113,6 +153,8 @@
                   action = "<cmd>split<cr>";
                   options.desc = "Horizontal Window Split";
                 }
+
+                # Ctrl Directional Window Jumps
                 {
                   mode = "n";
                   key = "<C-h>";
