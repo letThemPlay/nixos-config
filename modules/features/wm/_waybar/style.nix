9f0be3afc5242559b8c39c77d65af98ae76f75e0
kelvin <kelvin.d.morris@gmail.com>
2026-07-02 12:16:37 +0100

    Adding nerd-fonts to core

--- modules/features/wm/_waybar/style.nix
+++ modules/features/wm/_waybar/style.nix
@@ -5,12 +5,15 @@
 
   /* Global bar background - Base16 Dark Neutral */
   window#waybar {
-      background-color: @transparent-base;
-      border-bottom: 2px solid @base01;
-      color: @base05;
-      transition-property: background-color;
-      transition-duration: .5s;
+    background-color: @transparent-base;
+    border-bottom: 2px solid @base01;
+    color: @base05;
+    transition-property: background-color;
+    transition-duration: .5s;
+
+    font-family: "JetBrains Mono", "Symbols Nerd Font Mono", sans-serif;
   }
+
 
   /* Universal module configuration container spacing padding */
   #workspaces,
