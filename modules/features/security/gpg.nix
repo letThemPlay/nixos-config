869de24a664274f1a00fb30af895278d24447580
kelvin <kelvin.d.morris@gmail.com>
2026-07-06 01:04:47 +0100

    Refactor on the gpg.nix feature file

--- modules/features/security/gpg.nix
+++ modules/features/security/gpg.nix
@@ -1,29 +1,22 @@
 { importAscFiles, ... }: {
-  flake.nixosModules.gpg =
-    { config, lib, ... }:
-    {
-      config = {
-        home-manager.users = lib.mapAttrs (
-          _: profile:
-          let
-            userKeys = importAscFiles profile.username;
-          in
-          if userKeys != [ ] then
-            {
-              programs.gpg = {
-                enable = true;
-                publicKeys = userKeys;
-              };
+  flake.nixosModules.gpg = { config, lib, ... }: {
+    config = {
+      home-manager.users = lib.mapAttrs (
+        _: profile:
+        let
+          userKeys = profile.username |> importAscFiles;
+        in
+        lib.optionalAttrs (userKeys != [ ]) {
+          programs.gpg = {
+            enable = true;
+            publicKeys = userKeys;
+          };
 
-              home.file.".ssh/config".text = ''
-                Match host * exec "gpg-connect-agent UPDATESTARTUPTTY /bye"
-              '';
-            }
-          else
-            {
-              _module.args = { };
-            }
-        ) config.ltp.users.registry;
-      };
+          home.file.".ssh/config".text = ''
+            Match host * exec "gpg-connect-agent UPDATESTARTUPTTY /bye"
+          '';
+        }
+      ) config.ltp.users.registry;
     };
+  };
 }
