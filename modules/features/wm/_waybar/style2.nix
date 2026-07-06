f99322b9ed0c7cb24882f81c62afa8f50d5569f1
kelvin <kelvin.d.morris@gmail.com>
2026-07-06 10:45:44 +0100

    Waybar changes

--- modules/features/wm/_waybar/style2.nix
+++ modules/features/wm/_waybar/style2.nix
@@ -1,12 +1,17 @@
 ''
   @define-color transparent-base alpha(@base00, 0.85);
 
-  * {
-      border: none;
-      border-radius: 0;
-      min-height: 0;
-      margin: 3px 3px 3px 3px;
-      padding: 0;
+  window#waybar {
+    background-color: transparent;
+    color: u/fg;
+    border-radius: 8px;
+  }
+
+  #waybar > box {
+    margin: 4px 8px 0 8px;
+    padding: 2px;
+    background-color: u/bg;
+    border-radius: 8px;
   }
 
   .modules-left,
