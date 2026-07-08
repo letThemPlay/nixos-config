f7b9b127d58a4be1ac6914a399d7f11d04e9d033
kelvin <kelvin.d.morris@gmail.com>
2026-07-08 13:14:27 +0100

    Update for impermanence

--- modules/features/system/impermanence.nix
+++ modules/features/system/impermanence.nix
@@ -4,45 +4,51 @@
       inputs.impermanence.nixosModules.impermanence
     ];
 
-    fileSystems."/persist".neededForBoot = true;
+    config = {
+      fileSystems."/persist".neededForBoot = true;
 
-    environment.persistence."/persist" = {
-      hideMounts = true;
-      directories = [
-        "/etc/ssh"
-        "/var/log"
-        "/var/lib/bluetooth"
-        "/var/lib/NetworkManager"
-        "/etc/secureboot"
-        "/var/lib/nixos"
-      ];
-      files = [
-        "/etc/machine-id"
+      environment.persistence."/persist" = {
+        hideMounts = true;
+        directories = [
+          "/var/log"
+          "/var/lib/bluetooth"
+          "/var/lib/NetworkManager"
+          "/etc/secureboot"
+          "/var/lib/nixos"
+          "/etc/ssh"
+          "/var/lib/systemd"
+        ];
+        files = [
+          "/etc/machine-id"
+          "/etc/passwd"
+          "/etc/shadow"
+          "/etc/group"
+        ];
+      };
+
+      home-manager.backupFileExtension = "hm-bak";
+
+      home-manager.sharedModules = [
+        (_: {
+          home.persistence."/persist" = {
+            directories = [
+              "Downloads"
+              "Music"
+              "Pictures"
+              "Documents"
+              "src"
+              ".local/share/niri"
+              ".local/share/keyrings"
+              ".config/pulse"
+              ".mozilla"
+              ".config/discord"
+            ];
+            files = [
+              ".zsh_history"
+            ];
+          };
+        })
       ];
     };
-
-    home-manager.sharedModules = [
-
-      (_: {
-        home.persistence."/persist" = {
-
-          directories = [
-            #"Downloads"
-            "Music"
-            "Pictures"
-            "Documents"
-            "src"
-            ".local/share/niri"
-            ".local/share/keyrings"
-            ".config/pulse"
-            ".mozilla"
-            ".config/discord"
-          ];
-          files = [
-            ".zsh_history"
-          ];
-        };
-      })
-    ];
   };
 }
