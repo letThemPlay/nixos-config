44f33daad073d060123ea2689429260a8d219d28
kelvin <kelvin.d.morris@gmail.com>
2026-07-08 12:53:25 +0100

    Reverted back to previous version and switch to using osConfig

--- modules/features/apps/git.nix
+++ modules/features/apps/git.nix
@@ -2,41 +2,51 @@ _: {
   flake.nixosModules.git =
     {
       pkgs,
-      config,
-      lib,
       ...
     }:
     {
       config = {
         environment.systemPackages = [ pkgs.git ];
 
-        home-manager.users = lib.mapAttrs (_: profile: {
-          programs.git = {
-            enable = true;
-            userName = profile.fullName or "";
-            userEmail = profile.email or "";
-          };
+        home-manager.sharedModules = [
+          (
+            { config, osConfig, ... }:
+            let
+              userProfile = osConfig.ltp.users.registry.${config.home.username} or { };
+            in
+            {
+              programs.git = {
+                enable = true;
+                settings = {
+                  user = {
+                    name = userProfile.fullName or "";
+                    email = userProfile.email or "";
+                  };
+                };
+              };
 
-          programs.zsh.shellAliases = {
-            g = "git";
-            gs = "git status -sb";
-            ga = "git add";
-            gaa = "git add --all";
-            gc = "git commit -m";
-            gca = "git commit --amend";
-            gp = "git push";
-            gpf = "git push --force-with-lease";
-            gl = "git pull";
-            gd = "git diff";
-            gb = "git branch";
-            gco = "git checkout";
-            gcb = "git checkout -b";
-            gsw = "git switch";
-            gsc = "git switch -c";
+              programs.zsh.shellAliases = {
+                g = "git";
+                gs = "git status -sb";
+                ga = "git add";
+                gaa = "git add --all";
+                gc = "git commit -m";
+                gca = "git commit --amend";
+                gp = "git push";
+                gpf = "git push --force-with-lease";
+                gl = "git pull";
+                gd = "git diff";
+                gb = "git branch";
+                gco = "git checkout";
+                gcb = "git checkout -b";
+                gsw = "git switch";
+                gsc = "git switch -c";
 
-            glog = "git log --graph --oneline --decorate --all";
-          };
-        }) config.ltp.users.registry;
+                glog = "git log --graph --oneline --decorate --all";
+              };
+            }
+          )
+        ];
       };
     };
 }
